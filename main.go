4dde14ea9a706f6a307f755b5ea7ee7fd4fa5ea4
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-12-03 18:12:37 +0200

    added basic http server with index page

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+func main() {
+
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("static/index.html"))
+	tmpl.Execute(w, nil)
+}
